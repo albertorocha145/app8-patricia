5f951ec9c845dc234c48b8392c28d45b0d8ff038
albertorocha145 <albertorocharincon08@gmail.com>
2017-05-21 17:47:23 -0500

    Revert "Revert "4sa""
    This reverts commit a5af70be6f0fd7cbbdc0342b772073657baaebf0.

---
+++ js/acciones7.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+  document.addEventListener("deviceready",function(){
+	  
+	  $('#izquierda').on("swipeleft",function(){
+		  navigator.notificacion.alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar"});
+});
+
+$('#derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notificacion.beep(1);
+			break;
+			
+			case 2:
+			navigator.notificacion.beep(1000);
+			break;
+		}
+	},"Aplicacion7","Beep,Vibrar,Cnacelar");
+	
+});
+  },false);
+});
+			
+			
